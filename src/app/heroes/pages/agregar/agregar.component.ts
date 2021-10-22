09ab947b891a50d26c44000ac70cab9643414053
javieragr <javieragr_1993@hotmail.com>
2021-10-21 21:14:59 -0700

    CRUD heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
@@ -6,10 +10,65 @@ import { Component, OnInit } from '@angular/core';
   
 })
 export class AgregarComponent implements OnInit {
+  publishers=[
+    {
+      id:'DC Comics',
+      desc:'DC - Comics'
 
-  constructor() { }
+    },
+    {
+      id:'Marvel Comics',
+      desc:'Marvel - Comics'
+
+    }
+  ]
+  heroe : Heroe={
+    superhero:'',
+    alter_ego:'',
+    characters:'',
+    first_appearance:'',
+    publisher:Publisher.DCComics,
+    alt_image:''
+
+  }
+
+  constructor(private heroeService:HeroesService,private activatedRoute:ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
+
+    if (!this.router.url.includes('editar')) {
+      return //validacion para que cuando este agregando uno nuevo y no sea editar no se mande llamar el metodo de get
+    }
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id})=>  this.heroeService.getHeroePorId(id))
+    ).subscribe(resp=> this.heroe=resp)
+  }
+
+  guardar(){
+    if (this.heroe.superhero.trim().length===0) {
+      return
+    }
+    if (this.heroe.id) {
+      
+      this.heroeService.actualizarHeroe(this.heroe).subscribe(resp=>{
+        console.log(resp);
+      })
+
+    }else{
+      this.heroeService.agregarHeroe(this.heroe).subscribe(resp=>{
+        this.router.navigate(['/heroes/editar',resp.id])
+      })
+    }
+    
+  }
+
+  eliminar(){
+
+    this.heroeService.borrarHeroe(this.heroe.id!).subscribe(resp=> {
+      this.router.navigate(['/heroes/agregar'])
+
+    })
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,7 +11,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-
+import {MatSelectModule} from '@angular/material/select';
 
 
 
@@ -32,7 +32,8 @@ import {MatInputModule} from '@angular/material/input';
     MatGridListModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
